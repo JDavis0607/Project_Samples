7c3c738f40256f6ab97241153092be04111715bd
Joel Davis <88343168+JDavis0607@users.noreply.github.com>
2025-09-05 12:25:38 -0400

    Initial Commit

---
+++ DVDInventory/Installer/Installer/Form1.Designer.vb
@@ -0,0 +1,184 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.Install = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+        Me.Cancel = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label2
+        '
+        Me.Label2.BackColor = System.Drawing.Color.White
+        Me.Label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(0, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(496, 60)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "       Please read the End-User License Agreement and check the box to state " & _
+    "that you agree to the" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "       Terms of Use."
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.CheckAlign = System.Drawing.ContentAlignment.TopLeft
+        Me.CheckBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 6.5!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CheckBox1.Location = New System.Drawing.Point(182, 259)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(119, 16)
+        Me.CheckBox1.TabIndex = 2
+        Me.CheckBox1.Text = "Yes, I agree to the "
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'Install
+        '
+        Me.Install.Font = New System.Drawing.Font("Courier New", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Install.Location = New System.Drawing.Point(328, 259)
+        Me.Install.Name = "Install"
+        Me.Install.Size = New System.Drawing.Size(75, 23)
+        Me.Install.TabIndex = 3
+        Me.Install.Text = "Update"
+        Me.Install.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.White
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(220, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "MCS Alumni Status Update Installer..."
+        '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.Location = New System.Drawing.Point(179, 273)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(149, 13)
+        Me.LinkLabel1.TabIndex = 1
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "End-User Lecense Agreement"
+        '
+        'Cancel
+        '
+        Me.Cancel.Font = New System.Drawing.Font("Courier New", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Cancel.Location = New System.Drawing.Point(409, 259)
+        Me.Cancel.Name = "Cancel"
+        Me.Cancel.Size = New System.Drawing.Size(75, 23)
+        Me.Cancel.TabIndex = 4
+        Me.Cancel.Text = "Cancel"
+        Me.Cancel.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.Font = New System.Drawing.Font("Georgia", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(193, 60)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(302, 177)
+        Me.Label4.TabIndex = 0
+        Me.Label4.Text = resources.GetString("Label4.Text")
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.BackColor = System.Drawing.SystemColors.Control
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(193, 65)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(92, 13)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Did you Know?"
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Checked = True
+        Me.CheckBox2.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.CheckBox2.Location = New System.Drawing.Point(12, 258)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(143, 17)
+        Me.CheckBox2.TabIndex = 7
+        Me.CheckBox2.Text = "Create Desktop Shortcut"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.Transparent
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(0, 63)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(188, 178)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 5
+        Me.PictureBox1.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(496, 294)
+        Me.Controls.Add(Me.CheckBox2)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Cancel)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Install)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.LinkLabel1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Update Setup - Massillon Christian School Alumni Status"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents CheckBox1 As System.Windows.Forms.CheckBox
+    Friend WithEvents Install As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents LinkLabel1 As System.Windows.Forms.LinkLabel
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Cancel As System.Windows.Forms.Button
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents CheckBox2 As System.Windows.Forms.CheckBox
+
+End Class
---
+++ DVDInventory/Installer/Installer/Form1.vb
@@ -0,0 +1,68 @@
+﻿// © 2025 Joel Davis. All rights reserved.
+// No part of this code may be used, copied, distributed,
+// or modified in any form without prior written permission.
+
+Public Class Form1
+    Public ProgName As String = "MCS Alumni Status" : Dim ShortName As String = "MCSAS"
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Install.Enabled = False
+        If IO.File.Exists(My.Computer.FileSystem.SpecialDirectories.ProgramFiles & "\Davisbros Technologies\MCS Alumni Status\UnInstall.exe") Then
+            Try
+                IO.File.Move(Application.StartupPath & "\credentials.MCSAS", My.Computer.FileSystem.SpecialDirectories.ProgramFiles & "\credentials.MCSAS")
+                IO.File.Move(Application.StartupPath & "\Directory.MCSAS", My.Computer.FileSystem.SpecialDirectories.ProgramFiles & "\Directory.MCSAS")
+            Catch
+            End Try
+        End If
+        Try
+            My.Computer.FileSystem.DeleteDirectory(My.Computer.FileSystem.SpecialDirectories.ProgramFiles & "\MCS Alumni Status\Application Files", FileIO.DeleteDirectoryOption.DeleteAllContents)
+            IO.File.Delete(My.Computer.FileSystem.SpecialDirectories.ProgramFiles & "\MCS Alumni Status\MCS Alumni Status.application")
+        Catch
+        End Try
+    End Sub
+    Private Sub CheckBox1_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles CheckBox1.CheckedChanged
+        If CheckBox1.Checked Then
+            Install.Enabled = True
+        ElseIf CheckBox1.Checked = CheckState.Unchecked Then
+            Install.Enabled = False
+        End If
+    End Sub
+    Private Sub LinkLabel1_LinkClicked(ByVal sender As System.Object, ByVal e As System.Windows.Forms.LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
+        Process.Start("http://Davis.techdweebs.com/Logs/EULA.htm")
+    End Sub
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Install.Click
+        Dim OpenWindows() As Process = System.Diagnostics.Process.GetProcessesByName("explorer")
+        For Each proc In OpenWindows
+            proc.Kill()
+        Next
+        Dim loc As String : Dim os As OperatingSystem
+        os = Environment.OSVersion
+        If os.Version.Major > 5 Then
+            loc = "C:\Users\" & Environment.UserName & "\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\" & ProgName
+        Else
+            loc = "C:\Documents and Settings\" & Environment.UserName & "\Start Menu\Programs\" & ProgName
+        End If
+        If IO.File.Exists(My.Computer.FileSystem.SpecialDirectories.ProgramFiles & "\Davisbros Technologies\" & ProgName & "\UnInstall.bat") Then
+            Try
+                IO.File.Delete(My.Computer.FileSystem.SpecialDirectories.Desktop & "\" & ProgName & ".url")
+                My.Computer.FileSystem.DeleteDirectory(loc, FileIO.DeleteDirectoryOption.DeleteAllContents)
+                IO.File.Delete(My.Computer.FileSystem.SpecialDirectories.ProgramFiles & "\Davisbros Technologies\" & ProgName & "\" & ProgName & ".exe")
+                IO.File.Delete(My.Computer.FileSystem.SpecialDirectories.ProgramFiles & "\Davisbros Technologies\" & ProgName & "\MCS Logo.ico")
+                IO.File.Delete(My.Computer.FileSystem.SpecialDirectories.ProgramFiles & "\" & ShortName & "Uns.vbs")
+            Catch
+            End Try
+        End If
+        Me.Hide()
+        Form2.Show()
+    End Sub
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Cancel.Click
+        MsgBox("If you do not update now, then the next time you use" & vbNewLine _
+                 & "[" & ProgName & "] you will not be able to use previous versions." & vbNewLine & vbNewLine _
+                 & "This ensures that you have the best quality security, functionality, and performance.", MsgBoxStyle.Exclamation, ProgName & " Update: Warning - you may want to update now!")
+        If MsgBox("Are you sure you want to Cancel a " & ProgName & " Update?", MsgBoxStyle.YesNo) = MsgBoxResult.Yes Then
+            IO.File.Copy(Application.StartupPath & "\" & ProgName & " Update.exe", My.Computer.FileSystem.SpecialDirectories.Desktop & "\" & ShortName & " Update.exe")
+            MsgBox("A file has been generated to your Desktop titled [" & ShortName & " Update]," & vbNewLine _
+                     & "so you can Update later.")
+            End
+        End If
+    End Sub
+End Class
